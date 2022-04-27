ba7d35f00b391f635b5d543f8f10a942b8977fa4
ls8868 <103352242+ls8868@users.noreply.github.com>
2022-04-27 14:03:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,10 +114,7 @@ install_XrayR() {
         url="https://github.com/ls8868/XrayR/releases/download/0.8.0/XrayR-linux-${arch}.zip"
         echo -e "开始安装 XrayR v$1"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 XrayR v$1 失败，请确保此版本存在${plain}"
-            exit 1
-        fi
+        
         
        
         
